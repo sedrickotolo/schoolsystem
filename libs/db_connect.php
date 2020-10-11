0d9facf381b21f7d265e530eb6299b3b8bb78f9c
sedrickotolo <sedricksedu2@gmail.com>
2020-10-11 21:43:39 +0300

    commit one

---
+++ libs/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+$con=mysqli_connect("localhost","root","","sms");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+ ?>
